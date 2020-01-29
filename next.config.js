f2433aa4633709f1c3fc08bfe8a792c60895ccbe
Dominick <dominickveil@gmail.com>
2020-01-29 10:35:43 +0100

    Set absolute imports

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
+const path = require('path')
+
 module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     });
-
+    config.resolve.modules.push(path.resolve('./'))
     return config;
   }
 };
