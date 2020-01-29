3c854014be6c6b72370af8fe71c59f9bc3c3c55b
Dominick <dominickveil@gmail.com>
2020-01-29 13:03:24 +0100

    Add global style

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import {Normalize} from 'styled-normalize'
+import {createGlobalStyle} from 'styled-components'
 
 import Header from './Header'
 import Footer from './Footer'
 
+const GlobalStyle = createGlobalStyle`
+  a{
+    text-decoration: none;
+  }
+`
+
 const Layout = (props) => {
   return (
     <div>
+      <GlobalStyle />
       <Normalize />
       <Header />
       {props.children}
