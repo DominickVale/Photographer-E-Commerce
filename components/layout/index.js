87ce14d7325eda08a6050017220ba282c1510abf
Dominick <dominickveil@gmail.com>
2020-01-29 12:25:21 +0100

    Add normalize

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Normalize} from 'styled-normalize'
 
 import Header from './Header'
 import Footer from './Footer'
@@ -6,6 +7,7 @@ import Footer from './Footer'
 const Layout = (props) => {
   return (
     <div>
+      <Normalize />
       <Header />
       {props.children}
       <Footer />
