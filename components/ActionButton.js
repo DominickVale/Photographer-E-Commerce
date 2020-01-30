0cc7b25656d04049096710018ce9dc9312cb196b
Dominick <dominickveil@gmail.com>
2020-01-30 12:25:47 +0100

    Basic action button styling

--- components/ActionButton.js
+++ components/ActionButton.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+import {StyledActionButton} from 'styles'
 export const ActionButton = (props) => {
   return (
     <>
-    <button onClick={props.onClick}>{props.children || 'Order Now'}</button>
+    <StyledActionButton onClick={props.onClick}>{props.children || 'Order Now'}</StyledActionButton>
     </>
   )
 }
--- styles/index.js
+++ styles/index.js
@@ -4,3 +4,13 @@ export const Filler = styled.div`
 height: ${props => props.height};
 width: ${props => props.width || '10vw'};
 `
+
+export const StyledActionButton = styled.button`
+display: block;
+background-color: white;
+font-weight: 900;
+border: none;
+padding: 1rem 2rem;
+border: 1px solid black;
+margin: auto;
+`
