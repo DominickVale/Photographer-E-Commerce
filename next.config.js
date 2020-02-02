25f0162d2d493a01740d88599c6dfbfdb54b9434
Dominick <dominickveil@gmail.com>
2020-02-02 15:23:15 +0100

    Add dotenv and contentful

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
+require('dotenv').config()
 module.exports = {
   webpack(config) {
     config.module.rules.push({
@@ -8,6 +9,11 @@ module.exports = {
     });
     config.resolve.modules.push(path.resolve('./'))
     config.resolve.alias['api'] = path.join(__dirname, './pages/api')
+    config.resolve.alias['constants'] = path.join(__dirname, './constants')
     return config;
-  }
+  },
+  env: {
+    SPACE_ID: process.env.SPACE_ID,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN
+  },
 };
